54119c121951bbe21f030a30231052eb576fb015
6738y <132930194+6738y@users.noreply.github.com>
2023-06-06 10:48:22 -0600

    Add files via upload

---
+++ estructura repetitiva while c4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while (contador>100)           //mientras el contador sea menor a 100
+	{
+		printf("%d\n",contador); //escribe el contador
+		contador++;             // contador menos 1 y lo escribe
+	}                          //   y repite lo mismo hasta que el contador sea maror a 100
+}
