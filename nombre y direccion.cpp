62b4c75b48dad8beecec98efbf64ed4cbcbc0aec
6738y <132930194+6738y@users.noreply.github.com>
2023-06-19 22:02:12 -0600

    Add files via upload

---
+++ nombre y direccion.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+char mostrar_datos(char nombre,char direccion){
+	nombre= 'ximena toscano flores ';
+	direccion='candelaria teotlalpan';
+	
+	printf("%c",nombre);
+	printf("%c",direccion);
+	
+}
+
+int main(){
+	printf("%c",mostar_datos (nombre,direccion));
+}
+
+
+
+
+
+
+
+
+
